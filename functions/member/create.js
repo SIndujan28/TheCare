7600ea83d7f8ff1c9884b8342dc0a89dfe443d7b
SIndujan28 <30890973+SIndujan28@users.noreply.github.com>
2020-02-23 00:12:20 +0530

    add create member endpoint

---
+++ functions/member/create.js
@@ -0,0 +1,27 @@
+import * as dynamoDbLib from '../../libs/dynamodb-lib';
+import { success, failure } from '../../libs/response-lib';
+
+export async function main(event) {
+  const data = JSON.parse(event.body);
+
+  const params = {
+    TableName: process.env.memberTableName,
+    Item: {
+      memberId: event.requestContext.Identity.cognitoIdentityId,
+      fullName: data.fullName,
+      contactNumber: data.contactNumber,
+      address: data.address,
+      nic: data.nic,
+      propertyDocument: data.propertyDocument,
+      occupation: data.occupation,
+      incomeLevel: data.incomeLevel,
+      createdAt: Date.now(),
+    },
+  };
+  try {
+    await dynamoDbLib.call('put', params);
+    return success(params.Item);
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
