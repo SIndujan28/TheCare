c1225c1ad93938704028fde1a261c0f4741f5bcb
SIndujan28 <30890973+SIndujan28@users.noreply.github.com>
2020-02-24 20:35:28 +0530

    add create subscription and its test

---
+++ functions/member/update.js
@@ -0,0 +1,36 @@
+import * as dynamoDbLib from '../../libs/dynamodb-lib';
+import { success, failure } from '../../libs/response-lib';
+
+const generateUpdateQuery = (fields) => {
+  const exp = {
+    UpdateExpression: 'set',
+    ExpressionAttributeNames: {},
+    ExpressionAttributeValues: {},
+  };
+  Object.entries(fields).forEach(([key, item]) => {
+    exp.UpdateExpression += `#${key} = :${key},`;
+    exp.ExpressionAttributeNames[`#${key}`] = key;
+    exp.ExpressionAttributeValues[`:${key}`] = item;
+  });
+  exp.UpdateExpression = exp.UpdateExpression.slice(0, -1);
+  return exp;
+};
+
+export async function main(event) {
+  const body = JSON.parse(event.body);
+  const expression = generateUpdateQuery(body);
+  const params = {
+    TableName: process.env.memberTableName,
+    Key: {
+      memberId: event.requestContext.identity.cognitoIdentityId,
+    },
+    ...expression,
+    ReturnValues: 'ALL_NEW',
+  };
+  try {
+    const response = await dynamoDbLib.call('update', params);
+    return success(response.Item);
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
